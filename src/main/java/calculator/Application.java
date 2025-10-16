b7bef51a8c29e968ec56375c08a297aef58fca0c
김충현 <winwiewie@gmail.com>
2025-10-16 19:52:09 +0900

    커스덤 구분자 기능 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -37,6 +37,19 @@ public class Application {
             return sum;
         }
 
+        //커스텀 구분자
+        if (strInput.startsWith("//") && strInput.contains("\\n")) {
+            String[] split = strInput.split("n");
+            String customValue = split[0].replace("//", "").replace("\\", "");
+
+
+            String[] strNumbers = split[1].split( "\\"+ customValue); //구분 문자 처리하기 위해서 \\ 추가
+            for (String strNumber : strNumbers) {
+                sum += Integer.parseInt(strNumber);
+            }
+            return sum;
+        }
+
         // 엔터만 입력시 결과:0
         return sum;
     }
