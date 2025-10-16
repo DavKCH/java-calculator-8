c9c66dc4669ea13a4859a3a0ce97f820769e21c6
김충현 <winwiewie@gmail.com>
2025-10-16 19:22:09 +0900

    문자열 입력후 더하는 계산기 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        int sum = calculator(Console.readLine());
+        System.out.println("결과 : " + sum);
+
+    }
+
+    private static int calculator(String strInput) {
+        int sum = 0;
+
+        // 숫자만 입력 햇을때
+        String trueNumbers ="[0-9]+";
+        if (strInput.matches(trueNumbers)) {
+            String[] strNumbers = strInput.split("");
+
+            for (String strNumber : strNumbers) {
+                sum += Integer.parseInt(strNumber);
+            }
+
+            return sum;
+        }
+
+        // 엔터만 입력시 결과:0
+        return sum;
     }
 }
